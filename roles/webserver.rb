e330b2934e5faf74cd46e2a3dd6bb193cf1f1274
Kylie Stradley <kylie.stradley@gmail.com>
2015-02-20 11:58:30 -0500

    add web server role, include base role in its run_list

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -1,11 +1,14 @@
 name "webserver"
 description "Web Server"
-run_list "recipe[apache]"
+run_list "role[base]", "recipe[apache]"
 default_attributes({
   "apache" => {
     "sites" => {
       "admin" => {
         "port" => 8000
+      },
+      "bears" => {
+        "port" => 8081
       }
     }
   }
